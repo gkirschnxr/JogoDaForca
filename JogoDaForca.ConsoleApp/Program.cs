708ee9f3e55b776e00a25ce2a62557936465c6a3
Gabriel Kirschner <kirschnxr@gmail.com>
2025-03-13 14:51:18 -0300

    Jogo da Forca v1 - todas as funcionalidades implementadas

--- JogoDaForca.ConsoleApp/Program.cs
+++ JogoDaForca.ConsoleApp/Program.cs
@@ -14,6 +14,7 @@
                 "BACURI",
                 "BANANA",
                 "CAJA",
+                "TANGERINA",
                 "CAJU",
                 "CARAMBOLA",
                 "CUPUACU",
